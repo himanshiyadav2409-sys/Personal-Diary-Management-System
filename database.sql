ca4d04fea018c1b898b20fc2a8720dab8cb63567
Himanshi Yadav <himanshiyadav2409@gmail.com>
2026-09-04 17:57:32 +0530

    Add Database setup file

---
+++ database.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE Personal_Diary_Management;
+
+USE Personal_Diary_Management;
+
+CREATE TABLE Personal_Diary (Name VARCHAR(20), Contact_No BIGINT PRIMARY KEY, Address VARCHAR(50), Email VARCHAR(30), DOB DATE, Anniversary DATE);
